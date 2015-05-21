08895f6c5f4f026b76a6d92ea195021bd07f297c
Alexander Solovyov <alexander.solovyov1337@gmail.com>
2015-05-21 19:01:24 +0300

    Added doxygen

--- Second_Semester/7/2/list.h
+++ Second_Semester/7/2/list.h
@@ -1,16 +1,22 @@
 #pragma once
 #include "../../../Some_Useful_Things/Exceptions/missingelement.h"
 
+/// @brief The List class - just the list
 class List
 {
 public:
     List();
     ~List();
 
+    /// @brief add - adds new element in the list
     void add(int value);
+    /// @brief remove - removes element or throws the exception
     void remove(int value);
+    /// @brief exists - checks if the element exists
     bool exists(int value) const;
+    /// @brief getLength - returns the number of elements
     int getLength() const;
+    /// @brief isEmpty - checks if the list is empty
     bool isEmpty() const;
 protected:
     class Node
@@ -21,6 +27,10 @@ protected:
         int value;
     } *guard;
 
+    /**
+     * @brief findElement - support function for exists-function and remove-function
+     * @return the Node that points at the element with the same value as the value passed to the function
+     */
     Node *findElement(int value) const;
     int length;
 };
--- Second_Semester/7/2/uniquelist.h
+++ Second_Semester/7/2/uniquelist.h
@@ -2,11 +2,13 @@
 #include "list.h"
 #include "../../../Some_Useful_Things/Exceptions/elementexists.h"
 
+/// @brief The UniqueList class - the list without repeat.
 class UniqueList : public List
 {
 public:
     UniqueList();
     ~UniqueList();
 
+    /// @brief add - adds new element or throws exception if that element exists
     void add(int value);
 };
