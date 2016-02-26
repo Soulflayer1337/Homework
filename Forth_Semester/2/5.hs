d5143dd0b2a0e518fd5652e4799f2c58a1fd8701
Alexander Solovyov <alexander.solovyov1337@gmail.com>
2016-02-26 12:38:41 +0300

    Added two approaches

--- Forth_Semester/2/5.hs
+++ Forth_Semester/2/5.hs
@@ -1,2 +1,15 @@
-isPalindrom :: String -> Bool
-isPalindrom str = str == (reverse str)
+-- Three approaches for palindrom problem
+-- The easiest way to solve the problem. Linear.
+isPalindrom1 :: String -> Bool
+isPalindrom1 str = str == (reverse str)
+
+-- A bit faster than first approach. Still linear
+isPalindrom2 :: String -> Bool
+isPalindrom2 str = take n str == take n (reverse str) where
+                   n = div (length str) 2
+
+-- Should be much slower than other solutions because of using init and last functions.
+isPalindrom3 :: String -> Bool
+isPalindrom3 [] = True
+isPalindrom3 [_] = True
+isPalindrom3 (fs:str1) = fs == (last str1) && isPalindrom3 (init str1)
